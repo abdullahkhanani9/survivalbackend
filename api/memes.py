455b5b7dcdfb0bcc1ea9d877a959931ab06a34ae
Djxnxnx <taruntata2008@gmail.com>
2024-01-19 08:42:22 -0800

    backend starting

--- api/memes.py
+++ api/memes.py
@@ -4,7 +4,7 @@ import requests
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
 
-from model.users import User
+
 
 memes_api = Blueprint('memes_api', __name__,
                    url_prefix='/api/memes')
@@ -12,18 +12,18 @@ memes_api = Blueprint('memes_api', __name__,
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
 api = Api(memes_api)
 
-class UserAPI:        
-    class _CRUD(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
-        def get(self, ingredients): # Create method
-            api_key = '84cfe45628de456c87a13a80b76f5bd8'  # Replace with your Spoonacular API key
-            url = f"https://api.spoonacular.com/recipes/findByIngredients?apiKey={api_key}&ingredients=" + ingredients
+class MemesAPI:     
+    class TopText(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
+        def get(self, top_text): # Create method
+            url = f"https://imaad08.github.io/tri-2-csp/2024/01/16/Temp_Frontend_IPYNB_2_.html"
             response = requests.get(url)
             if response.status_code == 200:
                 data = response.json()
                 return jsonify(data)
-            else:
-                #return jsonify({"error": "Failed to fetch recipes."}), response.status_code
-             return {'message': f'hello'}, 400
+    class BottomText(Resource):
+        def get(self, bottom_text):
+            bottomText = bottom_text
         
     
-    #api.add_resource(_CRUD, '/getrecipes/<ingredients>')
+    api.add_resource(TopText, '/get_text/<top_text>')
+    api.add_resource(BottomText, '/get_text/<bottom_text>')
