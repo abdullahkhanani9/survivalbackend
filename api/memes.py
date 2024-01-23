c0327b476e16438be2cad1c663667b787f05c4d0
RonitT1234 <ronit.joe@gmail.com>
2024-01-22 16:05:17 -0800

    potentially broken changes but all good no trouble in da hood

--- api/memes.py
+++ api/memes.py
@@ -15,11 +15,11 @@ api = Api(memes_api)
 class MemesAPI:     
     class TopText(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
         def get(self, top_text): # Create method
-           return top_text
+            return top_text
     class BottomText(Resource):
         def get(self, bottom_text):
-            bottomText = bottom_text
+            return bottom_text
         
     
-    api.add_resource(TopText, '/get_text/<top_text>')
-    api.add_resource(BottomText, '/get_text/<bottom_text>')
+    api.add_resource(TopText, '/get_toptext/<top_text>')
+    api.add_resource(BottomText, '/get_bottomtext/<bottom_text>')
