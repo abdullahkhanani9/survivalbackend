c6fe166946072db1b7cc2de705af52720d30ac85
abdullahkhanani9 <khananiabdullah321@gmail.com>
2024-03-20 11:04:56 -0700

    remove api requests

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ from projects.projects import app_projects # Blueprint directory import projects
 db.init_app(app)
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
