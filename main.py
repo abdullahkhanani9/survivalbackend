c448fb50e32c8ba1435098367edb5669b3df3af8
abdullahkhanani9 <khananiabdullah321@gmail.com>
2024-03-20 11:28:25 -0700

    mainpyfixtets

--- main.py
+++ main.py
@@ -1,51 +1,37 @@
 import threading
-
-# import "packages" from flask
-from flask import render_template,request  # import render_template from "public" flask libraries
+from flask import render_template, request
 from flask.cli import AppGroup
-
-
-# import "packages" from "this" project
-from __init__ import app, db, cors  # Definitions initialization
-
-
-# setup APIs
-from api.user import user_api # Blueprint import api definition
+from __init__ import app, db, cors
+from api.user import user_api
 from api.player import player_api
 from api.titanic import titanic_api
-# database migrations
 from model.users import initUsers
 from model.players import initPlayers
-
-# setup App pages
-from projects.projects import app_projects # Blueprint directory import projects definition
-
+from projects.projects import app_projects
 
 # Initialize the SQLAlchemy object to work with the Flask app instance
 db.init_app(app)
 
-# register URIs
-app.register_blueprint(user_api) # register api routes
+# Register URIs
+app.register_blueprint(user_api)
 app.register_blueprint(player_api)
-app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(app_projects)
 app.register_blueprint(titanic_api)
 
-@app.errorhandler(404)  # catch for URL not found
+@app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
-@app.route('/')  # connects default URL to index() function
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/table/')  # connects /stub/ URL to stub() function
+@app.route('/table/')
 def table():
     return render_template("table.html")
 
 @app.before_request
 def before_request():
-    # Check if the request came from a specific origin
     allowed_origin = request.headers.get('Origin')
     if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io']:
         cors._origins = allowed_origin
@@ -61,9 +47,7 @@ def generate_data():
 
 # Register the custom command group with the Flask application
 app.cli.add_command(custom_cli)
-        
-# this runs the application on the development server
+
+# Run the application
 if __name__ == "__main__":
-    # change name for testing
     app.run(debug=True, host="0.0.0.0", port="8086")
-    
